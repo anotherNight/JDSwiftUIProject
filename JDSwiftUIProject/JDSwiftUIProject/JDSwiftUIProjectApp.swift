da6028e842606305032c1e0cc32ece4528f818ee
吴俊东 <evansjdwu@kugou.net>
2022-01-04 15:18:19 +0800

    init

---
+++ JDSwiftUIProject/JDSwiftUIProject/JDSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  JDSwiftUIProjectApp.swift
+//  JDSwiftUIProject
+//
+//  Created by evansjdwu on 2022/1/4.
+//
+
+import SwiftUI
+
+@main
+struct JDSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
